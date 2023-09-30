245cab19a5733d0e4069256e78039a6cdf18953d
Vladimir <volosh.vs@gmail.com>
2023-09-30 11:44:04 +0300

    web  hook

--- index.js
+++ index.js
@@ -7,21 +7,35 @@ const token = config.get("token");
 
 const bot = new TelegramBot(token, { polling: true });
 
+const ngrokUrl = "https://254b-2a09-bac1-1380-00-49-bc.ngrok-free.app"; // Замените на ваш реальный URL от ngrok
+const webhookUrl = `${ngrokUrl}/webhook`;
+
+bot
+  .setWebHook(webhookUrl)
+  .then(() => {
+    console.log(`Webhook установлен на ${webhookUrl}`);
+  })
+  .catch((error) => {
+    console.error("Ошибка установки вебхука:", error);
+  });
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  b;
   res.json({ user: "volodia" });
 });
 
 // Обработка POST-запросов от Telegram
 app.post("/webhook", (req, res) => {
+  console.log("ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
   console.log("req.body", req.body);
   const { message } = req.body;
-  if (message) {
-    // Обрабатываем сообщение
-    console.log(`Получено сообщение: ${message}`);
+  if (message && message.chat && message.chat.id === -4024512546) {
+    // Обрабатываем сообщение только из указанной группы
+    console.log(`Получено сообщение из группы: ${message.text}`);
   }
-  console.log("");
+  console.log("не получено");
   res.json({ user: message });
 });
 
