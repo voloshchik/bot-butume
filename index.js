823eec093fbb38840fc57e94c65c5834fba66ad9
Vladimir <volosh.vs@gmail.com>
2023-09-30 15:11:43 +0300

    messagText includes

--- index.js
+++ index.js
@@ -1,48 +1,26 @@
-const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 const config = require("config");
-const bodyParser = require("body-parser");
-const app = express();
+
 const token = config.get("token");
 
-const bot = new TelegramBot(token);
+// Создайте экземпляр бота с использованием Long Polling
+const bot = new TelegramBot(token, { polling: true });
 
-const ngrokUrl = "https://254b-2a09-bac1-1380-00-49-bc.ngrok-free.app"; // Замените на ваш реальный URL от ngrok
-const webhookUrl = `${ngrokUrl}/webhook`;
+// Обработчик для входящих сообщений от Telegram
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
 
-const url = ngrokUrl;
+  const messageText = msg.text;
 
-bot
-  .setWebHook(`${url}/bot${token}`)
-  .then(() => {
-    console.log(`Webhook установлен на ${webhookUrl}`);
-  })
-  .catch((error) => {
-    console.error("Ошибка установки вебхука:", error);
-  });
-
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.json({ user: "volodia" });
-});
-
-// Обработка POST-запросов от Telegram
-
-const rthw = `/bot${token}`;
-app.post(rthw, (req, res) => {
-  console.log("ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
-  console.log("req.body", req.body);
-  const { message } = req.body;
-  if (message && message.chat && message.chat.id === -4024512546) {
-    // Обрабатываем сообщение только из указанной группы
-    console.log(`Получено сообщение из группы: ${message.text}`);
+  // Делаем что-то с полученным сообщением
+  if (messageText.includes("натяжные потолки") || messageText.includes("потолок")) {
+    console.log(`Получено сообщение от чата ${chatId} о натяжных потолках: ${messageText}`);
+    // Отправляем ответное сообщение
+    bot.sendMessage(chatId, "Мы специализируемся на натяжных потолках. Как мы можем вам помочь?");
   }
-  console.log("не получено");
-  res.json({ user: message });
+  // Отправляем ответное сообщение
+  b;
+  // bot.sendMessage(chatId, "Спасибо за ваше сообщение!");
 });
 
-// Запуск сервера на порту 3000
-app.listen(3000, () => {
-  console.log("Сервер запущен на порту 3000");
-});
+console.log("Бот запущен и слушает сообщения...");
